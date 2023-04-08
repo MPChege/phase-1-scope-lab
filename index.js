51bceb903e0fc41da4319c2f3752d4ca95c890d8
MPChege <mark.wanyoike@strathmore.edu>
2023-04-08 23:56:34 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Write your solution in this file!
+let customerName = 'bob';
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+module.exports = {
+    customerName,
+    upperCaseCustomerName
+  }
+  
+
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob';
+}
+
+function changeLeastFavoriteCustomer() {
+  const leastFavoriteCustomer = 'someone else';
+  leastFavoriteCustomer = 'new favorite';
+}
+
+module.exports = {
+  customerName,
+  upperCaseCustomerName,
+  setBestCustomer,
+  overwriteBestCustomer,
+  changeLeastFavoriteCustomer
+};
